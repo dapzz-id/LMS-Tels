1eb60524a5b86815ea2c102831ee47bfe9129a0c
unknown <kadaviradityaa@outlook.co.id>
2026-03-14 06:23:03 +0700

    Update minor bug 2 for prod

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -10,10 +10,8 @@ import StudentActivityIntegration from './Components/StudentActivityIntegration'
 import GlobalApiErrorHandler from './Components/GlobalApiErrorHandler';
 import { Toaster } from 'sonner';
 
-const appName = import.meta.env.VITE_APP_NAME ?? 'Laravel';
-
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title} - ${import.meta.env.VITE_APP_NAME}`,
     resolve: (name) => resolvePageComponent(
         `./Pages/${name}.tsx`,
         import.meta.glob('./Pages/**/*.tsx')
